08acfb5eae0ba59859277e8ff3f1966eed17b866
AKSHAY N <94807520+akshaynstack@users.noreply.github.com>
2023-01-26 00:59:41 +0530

    Create ParallelogramPattern.java

---
+++ ParallelogramPattern.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class Main {
+	
+	public static void main(String[] args) {
+		// Write your code here
+		Scanner sc = new Scanner(System.in);
+        int row = sc.nextInt();
+
+        for (int i = 1; i <= row; i++)
+        {
+            for (int j = 2; j <= i; j++)
+            {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= row; j++)
+            {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+    }
+}
