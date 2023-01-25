aa36d7cea5fbfa8c79d2eab729f24dafab371a7b
AKSHAY N <94807520+akshaynstack@users.noreply.github.com>
2023-01-26 01:00:34 +0530

    Create SumPattern.java

---
+++ SumPattern.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+public class Main {
+	
+	public static void main(String[] args) {
+		// Write your code here
+		Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int i, j, num;
+        
+    	for(i=1; i<=n; i++)
+    	{
+        	num=1;
+        	int sum=0;
+        	for(j=1; j<=i; j++)
+        	{
+            	System.out.print(num+ "");
+                sum=sum+num;
+            	if (num<i) 
+                {
+                	System.out.print("+");
+                }
+                
+            	num++;
+        	}
+            System.out.print("="+sum);
+            System.out.println();
+        }
+	}
+}
